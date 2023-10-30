c528c3aeb8302120f8ef7954e6b4a47ea17d048b
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-10-31 00:19:37 +0200

    Data persistence issue solved

--- app/src/main/java/com/example/phones/presentation/MainActivity.kt
+++ app/src/main/java/com/example/phones/presentation/MainActivity.kt
@@ -3,27 +3,20 @@ package com.example.phones.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.MyApplication
 import com.example.phones.R
 import com.example.phones.databinding.ActivityMainBinding
+import com.example.phones.utils.collectLatestLifeCycleFlow
+import com.example.phones.utils.collectLifeCycleFlow
 import com.example.phones.utils.visibleIf
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val phonesScreenViewModel: PhonesScreenViewModel by lazy {
-        (application as MyApplication).dependenciesProvider.myViewModelFactory.create(
-            PhonesScreenViewModel::class.java
-        )
-    }
+    private val phonesScreenViewModel: PhonesScreenViewModel by lazy { ViewModelProvider(this,(application as MyApplication).dependenciesProvider.myViewModelFactory)[PhonesScreenViewModel::class.java] }
     private lateinit var adapter: PhonesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,20 +37,8 @@ class MainActivity : AppCompatActivity() {
             if (it.phones.isNotEmpty()) { adapter.submitList(it.phones) }
             binding.progressBar visibleIf it.loading
         }
-
         collectLifeCycleFlow(phonesScreenViewModel.errorState) { errorState ->
             errorState.errorMessage.let { Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show() }
-        }
-    }
-    private fun <T> collectLatestLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) { flow.collectLatest(collect) }
-        }
-    }
-
-    private fun <T> collectLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) { flow.collect(collect) }
         }
     }
 }
--- app/src/main/java/com/example/phones/utils/utils.kt
+++ app/src/main/java/com/example/phones/utils/utils.kt
@@ -1,7 +1,18 @@
 package com.example.phones.utils
 
 import android.view.View
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-infix fun View.visibleIf(condition : Boolean){
-    visibility = if(condition) View.VISIBLE else View.INVISIBLE
+infix fun View.visibleIf(condition : Boolean){ visibility = if(condition) View.VISIBLE else View.INVISIBLE }
+fun <T> LifecycleOwner.collectLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch { repeatOnLifecycle(Lifecycle.State.STARTED) { flow.collect(collect) } }
+}
+fun <T> LifecycleOwner.collectLatestLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch { repeatOnLifecycle(Lifecycle.State.STARTED) { flow.collectLatest(collect) } }
 }
